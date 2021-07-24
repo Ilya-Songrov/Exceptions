982b74a8116f29b071cd28299fc6513929da11b6
songrov <ilyasongrov@gmail.com>
2021-07-24 19:51:52 +0300

    Added license

--- exceptions/unixsignalnotifier.h
+++ exceptions/unixsignalnotifier.h
@@ -1,5 +1,16 @@
-#ifndef UNIXSIGNALNOTIFIER_H
-#define UNIXSIGNALNOTIFIER_H
+/**************************************************************************
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/.
+**
+**************************************************************************/
+
+#pragma once
 
 #include <QObject>
 #include <QSocketNotifier>
@@ -29,5 +40,3 @@ signals:
     void signalUnix();
 
 };
-
-#endif // UNIXSIGNALNOTIFIER_H
--- main.cpp
+++ main.cpp
@@ -1,3 +1,15 @@
+/**************************************************************************
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 as published by the Free Software
+** Foundation with exceptions as appearing in the file LICENSE.GPL3
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/.
+**
+**************************************************************************/
+
 #include <QCoreApplication>
 #include <QCommandLineParser>
 #include <QTimer>
@@ -9,7 +21,7 @@ int main(int argc, char *argv[])
 {
     try {
         ApplicationNotify<QCoreApplication> app(argc, argv);
-        QCoreApplication::setApplicationVersion("0.0.2");
+        QCoreApplication::setApplicationVersion("0.0.3");
 
         QCommandLineParser parser;
         parser.setApplicationDescription("App description");
